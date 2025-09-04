641521d53c8a439ff19c05ba6773338360daa2e5
vanthang123 <vanthang2222147@gmail.com>
2025-09-04 14:59:37 +0700

    deploy to render

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+
+// Render sẽ cấp biến môi trường PORT
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello Teacher I AM Nguyen Van Thang lop ST22D /h1>');
+  res.end('<h1>Hello Teacher, I am Nguyen Van Thang - Lớp ST22D</h1>');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+server.listen(PORT, () => {
+  console.log(`✅ Server running at port ${PORT}`);
 });
