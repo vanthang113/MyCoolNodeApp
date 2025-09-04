d3d0c4e10b2717b7fcdbc42ae9a88a28a714fc02
vanthang123 <vanthang2222147@gmail.com>
2025-09-04 14:56:48 +0700

    deploy to render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello Teacher I AM Nguyen Van Thang lop ST22D /h1>');
 });
 
 server.listen(port, () => {
